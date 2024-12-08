659f38912133c5b6223393cf438b4d6ec043c83d
Yordan Kanchelov <yordan.kanchelov@gmail.com>
2024-12-09 01:28:16 +0200

    chore: update lint tools & project dependencies (#1442)
    * chore: remove husky pre-commit hook and ESLint configuration files; update package.json and add new ESLint config
    
    * chore: update VSCode launch configuration to use Edge and change task script to start
    
    * chore: remove lint-staged configuration and update package.json scripts
    
    * chore: update dependencies in package.json to latest versions
    
    * chore: remove file extensions from ESLintPlugin configuration in webpack files
    
    * chore: update package-lock.json with latest dependency versions
    
    * chore: update Node.js version in GitHub Actions workflow to 22.x
    
    * chore: add ESLint plugins and update TypeScript project configuration

--- src/utils/spine-example.ts
+++ src/utils/spine-example.ts
@@ -1,11 +1,11 @@
 import { Spine } from "pixi-spine";
-
 import { Assets } from "pixi.js";
 
 export async function getSpine(): Promise<Spine> {
     const spineResource = await Assets.get("pixie");
 
     const spineExample = new Spine(spineResource.spineData);
+
     spineExample.scale.set(0.3, 0.3);
     spineExample.y = spineExample.height * 1.1;
     spineExample.x = spineExample.width / 2;
