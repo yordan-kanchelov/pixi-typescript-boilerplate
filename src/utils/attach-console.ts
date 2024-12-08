c56e5467fa384c37385b3324773d0e5ba4ac8751
yordan.kanchelov <yordan.kanchelov@gmail.com>
2024-12-09 01:22:14 +0200

    chore: add spacing for improved readability in index.ts, attach-console.ts, and spine-example.ts

--- src/utils/attach-console.ts
+++ src/utils/attach-console.ts
@@ -3,12 +3,14 @@ import { Container } from "pixi.js";
 
 export function attachConsole(stage: Container, width: number, height: number): void {
     const consoleConfig = new PixiConsoleConfig();
+
     consoleConfig.consoleWidth = width;
     consoleConfig.consoleHeight = height / 2.5;
     consoleConfig.showCaller = true;
     consoleConfig.fontSize = 15;
 
     const pixiConsole = new PixiConsole(consoleConfig);
+
     pixiConsole.show();
 
     stage.addChild(pixiConsole);
