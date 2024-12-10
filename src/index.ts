5a01a42dbfff24ef8d20c7e247674b174b1b3427
yordan.kanchelov <yordan.kanchelov@gmail.com>
2024-12-10 23:10:09 +0200

    feat: replace pixie character with spineBoy and update associated assets

--- src/index.ts
+++ src/index.ts
@@ -60,19 +60,17 @@ async function loadGameAssets(): Promise<void> {
                 ],
             },
             {
-                name: "pixie",
+                name: "spineBoy",
                 assets: [
-                    {
-                        name: "pixie",
-                        srcs: "./assets/spine-assets/pixie.json",
-                    },
+                    { alias: "spineboyData", src: "./assets/spine-assets/spineboy-pro.skel" },
+                    { alias: "spineboyAtlas", src: "./assets/spine-assets/spineboy.atlas" }
                 ],
             },
         ],
     };
 
     await Assets.init({ manifest });
-    await Assets.loadBundle(["bird", "pixie"]);
+    await Assets.loadBundle(["bird", "spineBoy"]);
 }
 
 function resizeCanvas(): void {
